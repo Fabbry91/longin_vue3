f4165a5b6d31548888b363d0ea88b5dfd81f25bb
Fabbry91 <fabbry1991@gmail.com>
2023-10-11 20:57:41 -0300

    correccion de url

--- frontend/src/services/config.ts
+++ frontend/src/services/config.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: "https://back-user-pzgk.onrender.com",
+  baseURL: "https://back-user-pzgk.onrender.com/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
