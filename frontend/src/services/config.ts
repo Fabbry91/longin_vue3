e351120366e5292b0d2b55f6402c5568f79ab559
Fabbry91 <fabbry1991@gmail.com>
2023-10-11 17:30:55 -0300

    correccion de errores

--- frontend/src/services/config.ts
+++ frontend/src/services/config.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: process.env.URL || "http://localhost:3000/api/v1/",
+  baseURL: "http://localhost:3000/api/v1/",
   headers: {
     "Content-Type": "application/json",
   },
